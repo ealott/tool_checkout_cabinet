8df65274242e0d52514129fe1faccb33eefaa1cd
Evan Lott <ealott@gmail.com>
2021-05-31 01:52:28 -0500

    Initital commit of working firmware

---
+++ firmware/main.py
@@ -0,0 +1,118 @@
+# TODO: PEP8 cleanup
+import network
+from usyslog import UDPClient, SyslogClient
+from machine import Pin, Timer
+import wifimgr 
+
+from wiegand import Wiegand #for reading 125khz RFID tags with the Wiegand protocol
+
+#For querying AD REST API
+import urequests as requests
+
+# TODO: move these settings to a config file
+#The AD group the member must be part of for access to be granted
+
+AD_Group="3D Printer Basics"
+syslog_server = '54.161.59.61' #'logs.evanlott.com'
+syslog_port = 514
+
+
+# configure the wifis
+# TODO: add extended configurations (syslog_server, AD_Group, etc.) to temporary Setup wifi page
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  
+else:    
+    nic = network.WLAN(network.STA_IF)
+    print("ESP OK")
+
+# TODO: Create variable for cabinet name, move to config file
+
+logger = UDPClient(ip=syslog_server, port=syslog_port)
+
+# TODO: Add cabinet name to settings, use name in logging     
+logger.log(5,"3DFabCab: Cabinet lock online at: " + nic.ifconfig()[0])
+logger.log(5,"3DFabCab: Cabinet lock access group: " + AD_Group)
+
+print(AD_Group)
+
+def has_access(rfid,AD_Group):
+    # TODO: Move lookup API URL to config file
+    url="http://192.168.200.32:8080/api/v1/lookupByRfid"
+    headers = {'cache-control':"no-cache",'content-type': "application/x-www-form-urlencoded",}
+    payload = 'rfid=' + str(rfid) 
+    response = requests.request("POST", url, data=payload, headers=headers)
+    if AD_Group in response.text:
+        #print("Access Granted; opening.").
+        return True
+    else:
+        #print("Access Denied")
+        return False
+
+def open_lock():
+    # TODO: move pin # and lock open period to to config file
+    pin = Pin(4, Pin.OUT)
+    
+    # Start a 2 second hardware timer to disable the solenoid (which causes the lock to latch again).  
+    # Engaging the lock for more than a few minutes will literally melt it.
+    relock_timer = Timer(0)
+    relock_timer.init(period=2000, mode=Timer.ONE_SHOT, callback=lambda t:pin.off())
+
+    pin.on()
+
+def get_lock_status():
+    # returns 1 when lock is unlatched, 0 when latched
+    #TODO: move pin number to config file
+    #TODO: setup interrupt handler to print and log when door lock status changes
+    pin = Pin(32, Pin.IN, Pin.PULL_UP)
+
+
+#Should produce 11094651804
+def wiegand26_decode(facility_code, card):
+    #wiegand library returns separate facility code and card number in decimal format.  
+    #To get the number printed on the card, we need to convert both to binary, concatenate, then convert back to decimal.
+    facility_code_bin="{0:b}".format(facility_code)                                                                                                                            
+    card_bin="{0:b}".format(card)                                                                                                                                                                                                                                                  
+    return int(facility_code_bin+card_bin,2)  
+
+def on_card(card_number, facility_code, cards_read):
+    decoded_rfid = str(wiegand26_decode(facility_code, card_number))
+    print('facility code: ' + str(facility_code) + '|' + 'card read:' + str(card_number) + '|' + str(cards_read) )
+    print("Decoded: " + decoded_rfid)
+    logger.log(5,'3DFabCab: rfid scanned: ' + decoded_rfid)
+
+    print("Checking access API...")
+    try:
+        if has_access(decoded_rfid, AD_Group):
+            print("Access Granted to " + AD_Group)
+            logger.log(5,"3DFabCab: Access Granted")
+            open_lock()
+
+
+        else:
+            print("Access Denied to " + AD_Group)
+            logger.log(5,"3DFabCab: Access Denied")
+
+    except Exception as E:
+        print(str(E))
+        f = open('error.txt', 'w')
+        f.write(str(E))
+        f.close()
+
+
+def main():
+    #Make sure the pin driving the solenoid's transistor is off:
+    pin = Pin(4, Pin.OUT)
+    pin.off()
+
+    #the Wiegand class will wait for IRQ interrupt on the specified pins, then execute the on_card callback function
+    WIEGAND_ZERO = 34  # D0 Pin number here
+    WIEGAND_ONE = 35  # D1 Pin number here
+
+    Wiegand(WIEGAND_ZERO, WIEGAND_ONE, on_card)
+
+    
+main()
+
